f478d7c7ad5fa72e3a3a60a2e087497d4b618cfa
amadesya <154593967+amadesya@users.noreply.github.com>
2025-06-24 10:44:30 +0300

    labwork30

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,90 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+using ProductService;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+using var chanel = GrpcChannel.ForAddress(config.GetSection("Sources")["Grps"]);
+var orderClient = new OrderService.OrderServiceClient(chanel);
+
+while (true)
+{
+    Console.WriteLine("1. Добавить заказ\n2. Получить заказ\n3. Удалить\n4. Все заказы\n5. Фильтрация\n0. Выход");
+    var input = Console.ReadLine();
+    switch (input)
+    {
+        case "1":
+            var order = new CreateOrderRequest()
+            {
+                Date = Timestamp.FromDateTime(DateTime.UtcNow)
+            };
+
+            Console.Write("Введите название товара:");
+            var name = Console.ReadLine();
+            Console.Write("Введите цену товара:");
+            var price = Console.ReadLine();
+
+            var product = new Product
+            {
+                Name = name,
+                Price = Convert.ToDouble(price)
+            };
+
+            order.Products.Add(product);
+
+            var result = await orderClient.CreateOrderAsync(order);
+            Console.WriteLine($"Создан заказ с ID: {result.Id}");
+            break;
+
+        case "2":
+            //Console.Write("Введите ID: ");
+            //var id = Console.ReadLine();
+            //var orderInfo = client.GetOrder(new OrderIdRequest { Id = id });
+            //Console.WriteLine($"Дата: {orderInfo.Date.ToDateTime()}, Товаров: {orderInfo.Items.Count}");
+            break;
+
+        case "3":
+            //Console.Write("Введите ID для удаления: ");
+            //id = Console.ReadLine();
+            //client.DeleteOrder(new OrderIdRequest { Id = id });
+            //Console.WriteLine("Удалено.");
+            break;
+
+        case "4":
+            OrderList reply = await orderClient.ListOrderAsync(new Empty());
+            foreach (var o in reply.Orders)
+            {
+                Console.WriteLine($"Заказ {o.Id}|{o.Date,20}|{o.Products.Count}");
+                foreach (var p in o.Products)
+                    Console.WriteLine($"# {p.Name,30}|{p.Price}");
+            }
+
+            break;
+
+        case "5":
+            //Console.Write("Мин. дата (YYYY-MM-DD): ");
+            //var date = DateTime.Parse(Console.ReadLine());
+            //Console.Write("Мин. сумма: ");
+            //var sum = double.Parse(Console.ReadLine());
+            //var filtered = client.FilterOrders(new FilterRequest
+            //{
+            //    Date = Timestamp.FromDateTime(date.ToUniversalTime()),
+            //    MinPrice = sum
+            //});
+            //foreach (var o in filtered.Orders)
+            //    Console.WriteLine($"ID: {o.Id}, Сумма: {o.Items.Sum(i => i.Price)}");
+            break;
+
+        case "0":
+            return;
+        default:
+            return;
+    }
+    Console.ReadLine();
+    Console.Clear();
+}
+
+
---
+++ GrpcServer/Services/OrderDataService.cs
@@ -0,0 +1,107 @@
+﻿using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using ProductService;
+
+namespace GrpcServer.Services
+{
+    public class OrderDataService : OrderService.OrderServiceBase
+    {
+        static int id = 0;
+        static List<Order> orders = new()
+        {
+            new Order() { Id = ++id, Date = DateTime.UtcNow.ToTimestamp(), Products =
+                {
+                    new Product() { Name = "Резной столик для чая", Price =  10000.00 },
+                    new Product() { Name = "IPhone 16 pro", Price =  10000.00 }
+                }
+            },
+            new Order() { Id = ++id, Date = DateTime.UtcNow.ToTimestamp(), Products =
+                {
+                    new Product() { Name = "Гречка", Price = 113.69 },
+                    new Product() { Name = "Маргарин", Price = 83.43 }
+                } },
+        };
+
+        static List<Product> ConvertRepeatedToProduct(RepeatedField<Product> productReply)
+            => productReply.Select(p => new Product() { Name = p.Name, Price = p.Price }).ToList();
+
+        public override Task<Order> CreateOrder(CreateOrderRequest request, ServerCallContext context)
+        {
+            var order = new Order() { Id = ++id, Date = request.Date };
+            order.Products.AddRange(request.Products);
+            orders.Add(order);
+            var reply = new Order() { Id = order.Id, Date = order.Date };
+            reply.Products.AddRange(order.Products);
+            return Task.FromResult(reply);
+        }
+
+        public override Task<Order> GetOrder(GetOrderRequest request, ServerCallContext context)
+        {
+            var order = orders.Find(o => o.Id == request.Id);
+            if (order == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "Order Not Found"));
+            }
+            var reply = new Order() { Id = order.Id, Date = order.Date };
+            reply.Products.AddRange(order.Products);
+
+            return Task.FromResult(reply);
+        }
+
+        public override Task<Order> UpdateOrder(UpdateOrderRequest request, ServerCallContext context)
+        {
+            var order = orders.Find(o => o.Id == request.Id);
+            if (order == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "Order Not Found"));
+            }
+            order.Products.AddRange(request.Products);
+
+            var reply = new Order() { Id = order.Id, Date = order.Date };
+            reply.Products.AddRange(order.Products);
+            return Task.FromResult(reply);
+        }
+
+        public override Task<Order> DeleteOrder(DeleteOrderRequest request, ServerCallContext context)
+        {
+            var order = orders.Find(o => o.Id == request.Id);
+            if (order == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "Order Not Found"));
+            }
+
+            orders.Remove(order);
+
+
+            var reply = new Order() { Id = order.Id, Date = order.Date };
+            reply.Products.AddRange(order.Products);
+            return Task.FromResult(reply);
+        }
+
+        public override Task<OrderList> ListOrder(Empty request, ServerCallContext context)
+        {
+            var reply = new OrderList();
+            var orderList = orders.Select(o =>
+            {
+                var order = new Order() { Id = o.Id, Date = o.Date };
+                order.Products.AddRange(o.Products);
+                return order;
+            });
+            reply.Orders.AddRange(orderList);
+            return Task.FromResult(reply);
+        }
+
+        public override Task<OrderList> FilterOrders(FilterRequest request, ServerCallContext context)
+        {
+            var result = orders
+                .Where(o => o.Date.ToDateTime() >= request.Date.ToDateTime() &&
+                            o.Products.Sum(i => i.Price) >= request.MinPrice)
+                .ToList();
+
+            var response = new OrderList();
+            response.Orders.AddRange(result);
+            return Task.FromResult(response);
+        }
+    }
+}
